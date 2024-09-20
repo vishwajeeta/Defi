ed03d0ffd3cbf60b7f8aa535b8c2fc2dcb6af98c
vishwajeeta <gs699542@gmail.com>
2024-09-20 21:13:08 +0530

    updated

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,1 +1,14 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/*
+ * @title Decentralized-Stable-Coin
+ * @author vishwa
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting:Algorithmic
+ * Relative Stability:pegged to USD
+ * governed by DSCEngine.
+*/
+contract DecentralizedStableCoin{
+
+}
