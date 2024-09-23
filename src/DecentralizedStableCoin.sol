ffbdca3bb3c22955b0ef0e3ad3ba3959d3cb1109
vishwajeeta <gs699542@gmail.com>
2024-09-23 17:54:23 +0530

    imported ERC20 smart contract

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 /*
  * @title Decentralized-Stable-Coin
  * @author vishwa
@@ -8,7 +8,7 @@ pragma solidity ^0.8.18;
  * Minting:Algorithmic
  * Relative Stability:pegged to USD
  * governed by DSCEngine.
-*/
-contract DecentralizedStableCoin{
-
+ */
+contract DecentralizedStableCoin is ERC20Burnable{
+    constructor() ERC20("DecentralizedStableCoin","DSC"){}
 }
