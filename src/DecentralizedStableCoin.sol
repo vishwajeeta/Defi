ac12807e16f17e729121f9f3edbb3817255150c3
vishwajeeta <gs699542@gmail.com>
2024-09-25 21:22:17 +0530

    added burning function of ERC20

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -13,7 +13,22 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract DecentralizedStableCoin is ERC20Burnable, Ownable{
 
     error DecentralizedStableCoin_MustBeMoreThanZero();
-    
+    error DecentralizedStableCoin_BurnAmountExceedsBalance();
+    error DecentralizedStableCoin_NotZeroAddress();
+
+    constructor() ERC20("DecentralizedStableCoin","DSC"){}
+
+    function burn(uint256 _amount)public override onlyOwner{
+        uint256 balance = balanceOf(msg.sender);
+        if(_amount <=0){
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        if(balance < _amount){
+            revert DecentralizedStableCoin_BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
     function mint(address _to,uint256 _amount)external onlyOwner returns(bool){
         if(_to == address(0)){
             revert DecentralizedStableCoin_NotZeroAddress();
