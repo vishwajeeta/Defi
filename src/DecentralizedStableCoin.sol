ac43261bfcf3f9c24e173fca57cd13865024c352
vishwajeeta <gs699542@gmail.com>
2024-09-25 21:21:00 +0530

    added minting function from ERC20

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /*
  * @title Decentralized-Stable-Coin
  * @author vishwa
@@ -9,6 +10,18 @@ import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensio
  * Relative Stability:pegged to USD
  * governed by DSCEngine.
  */
-contract DecentralizedStableCoin is ERC20Burnable{
-    constructor() ERC20("DecentralizedStableCoin","DSC"){}
+contract DecentralizedStableCoin is ERC20Burnable, Ownable{
+
+    error DecentralizedStableCoin_MustBeMoreThanZero();
+    
+    function mint(address _to,uint256 _amount)external onlyOwner returns(bool){
+        if(_to == address(0)){
+            revert DecentralizedStableCoin_NotZeroAddress();
+        }
+        if(_amount <= 0){
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        _mint(_to,_amount);
+        return true;
+    }
 }
