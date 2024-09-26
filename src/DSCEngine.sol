5b90886deedb45aafdba04868a94d8936133b709
vishwajeeta <gs699542@gmail.com>
2024-09-26 20:21:47 +0530

    properties of DSE

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -6,7 +6,12 @@ pragma solidity ^0.8.18;
  * @author Vishwa
  *
  * minimal contract with 1crypto==1$ peg.
- 
+ * This stablecoin has the properties:
+ * - Exogenous Collateral
+ * - Dollar Pegged
+ * - Algorithmically Stable
+
+ * @notice This contract is the core of the DSE System. it handles all the logic for minting and burning also redeeming DSC.+ depositing and withdrawing collateral.
 */
 
 contract DSCEngine{}
