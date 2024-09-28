dd6cf737737440ce5097a11998e237982037e3db
vishwajeeta <gs699542@gmail.com>
2024-09-28 16:58:52 +0530

    updated

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -17,7 +17,11 @@ pragma solidity ^0.8.18;
 
 contract DSCEngine{
     function depositCollateralAndMintDsc() external {}
+    function depositCollateral()external{}
     function redeemCollateralForDsc() external {}
+    function redeemCollateral()external{}
+    function mintDsc()external{}
     function burnDsc() external {}
-    
+    function lizuidate()external {}
+    function getHealthFactor() external view {}
 }
