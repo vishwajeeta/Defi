8520ab6c3b070ad07f64a0befad538e626b4f005
vishwajeeta <gs699542@gmail.com>
2024-10-30 21:57:38 +0530

    added mint function on single transaction both depostiCollateral and mint DSC

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -75,14 +75,24 @@ contract DSCEngine is ReentrancyGuard {
     }
 
     //----------------External functions---------------
-    function depositCollateralAndMintDsc() external {}
+
+    /**
+     * @param tokenCollateralAddress The address of the token to deposit as collateral
+     * @param amountCollateral The amount of collateral to deposit
+     * @param amountDscToMint The amount of decentralized stable coin to mint
+     * @note this function will deposit your collateral and mint DSC in one transaction
+     */
+    function depositCollateralAndMintDsc(address tokenCollateralAddress,uint256 amountCollateral,uint256 amountDscToMint) external {
+        depositCollateral(tokenCollateralAddress,amountCollateral);
+        mintDsc(amountDscToMint);
+    }
 
     /**
      * @param tokenCollateralAddress The address of the token to deposit as collateral
      * @param amountCollateral The amount of collateral to deposit
      */
     function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
-        external
+        public
         moreThanZero(amountCollateral)
         isAllowedToken(tokenCollateralAddress)
         nonReentrant
@@ -103,7 +113,7 @@ contract DSCEngine is ReentrancyGuard {
      * @notice They must have more collateral value than minimum threshold
      */
 
-    function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
+    function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) nonReentrant {
         s_DscMinted[msg.sender] += amountDscToMint;
         // if they minted too much ($150 DSC , $100 ETH)
         _revertIfHealthFactorIsBroken(msg.sender);
